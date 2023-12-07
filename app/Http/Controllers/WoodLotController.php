f4144f4365926d1652c1b1189c09d2fbc280bf84
Armanhozyn <armanhossen591@gmail.com>
2023-12-07 07:33:57 +0600

    hello

--- app/Http/Controllers/WoodLotController.php
+++ app/Http/Controllers/WoodLotController.php
@@ -157,6 +157,7 @@ class WoodLotController extends Controller
             ->when(!empty($range_or_center_lot_no_and_year), function ($query) use ($range_or_center_lot_no_and_year) {
                 return $query->where('range_lot_no_year', $range_or_center_lot_no_and_year);
             })
+            ->where('gardens.range_id',Auth::user()->range_id)
             ->latest('gardens.created_at')
             ->get();
 
